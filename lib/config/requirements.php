cb8417c770bfcb1ec9aa4ebae492c22fddd476e6
Serge Rodovnichenko <sergerod@gmail.com>
2015-08-03 02:49:42 +0300

    Уточнены требования

---
+++ lib/config/requirements.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @version 1.0.0
+ * @copyright Serge Rodovnichenko, 2015
+ * @license
+ */
+return array(
+    'php.curl'     => array(
+        'description' => 'Необходимо для запросов к API', 'strict' => true,
+    ),
+    'php.mbstring' => array(
+        'description' => 'Необходимо для сравнения строк в кодировке UTF-8', 'strict' => true,
+    ),
+    'php.simplexml' => array(
+        'description' => 'Необходимо для работы со справочником городов', 'strict' => true,
+    ),
+
+);
